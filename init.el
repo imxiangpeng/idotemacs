46c46efaba68a777e20bc5c970fd3d42ecb1281b
Alex Meng <mengxp@inspur.com>
2020-04-12 16:08:37 +0800

    disable auto backup
    Signed-off-by: Alex Meng <mengxp@inspur.com>

--- init.el
+++ init.el
@@ -23,12 +23,15 @@
 ;; hight current line
 (global-hl-line-mode 1);
 
+;; disable default backup
+(setq make-backup-files nil)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (helm-projectile helm))))
+ '(package-selected-packages (quote (magit helm-projectile helm))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
