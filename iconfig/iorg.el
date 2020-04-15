d67e478541c9ac90236b33341a0f39ee2c0a8829
Alex Meng <imxiangpneg@gmail.com>
2020-04-15 17:58:11 +0800

    default enable org indent mode
    Signed-off-by: Alex Meng <imxiangpneg@gmail.com>

--- iconfig/iorg.el
+++ iconfig/iorg.el
@@ -1,7 +1,13 @@
 (require 'org)
 
 (setq-default org-use-sub-superscripts nil)
-(
- org-indent-mode)
+
+
+(setq org-adapt-indentation t
+  org-hide-leading-stars t
+  org-odd-levels-only t)
+
+(with-eval-after-load 'org
+  (setq org-startup-indented t))
 
 (provide 'iorg)
