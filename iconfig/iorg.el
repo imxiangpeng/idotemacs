57ee2cc6c2dd0e97afc07ca0c6b1d1f67f1152af
Alex Meng <imxiangpneg@gmail.com>
2020-04-16 19:17:41 +0800

    disable backup
    Signed-off-by: Alex Meng <imxiangpneg@gmail.com>

--- iconfig/iorg.el
+++ iconfig/iorg.el
@@ -3,11 +3,14 @@
 (setq-default org-use-sub-superscripts nil)
 
 
-(setq org-adapt-indentation t
-  org-hide-leading-stars t
-  org-odd-levels-only t)
+;(setq org-adapt-indentation t
+;  org-hide-leading-stars t
+;  org-odd-levels-only t)
 
 (with-eval-after-load 'org
   (setq org-startup-indented t))
+
+; stop
+(setq org-export-with-sub-superscripts nil)
 
 (provide 'iorg)
